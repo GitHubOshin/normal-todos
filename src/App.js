a6d5a5ed1c06ff0e1bb17bc37e8632196fcebdd8
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2022-11-25 21:27:56 +0700

    Style "App" component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <header>
+        <h1>TODOS</h1>
+      </header>
+      <div>
+        <div>
+          <input />
+          <button>Add todo</button>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <span>Go to ABC party</span>
+              <button>x</button>
+            </li>
+          </ul>
+          <button>Clear all</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
